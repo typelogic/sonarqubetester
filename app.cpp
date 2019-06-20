143613793ab0a363352c6c43bce87eb1c4c9b016
dexter aparicio <da185157@ncr.com>
2019-06-20 20:25:05 +0000

    switch with no default case

--- app.cpp
+++ app.cpp
@@ -4,5 +4,15 @@ int main (int argc, char *argv[])
 {
     int i = 69;
     std::cout << "hello sonarqube!" << i << std::endl;
+    switch (i) {
+        case 0:
+        std::cout << "0";
+        break;
+
+        case 1:
+        std::cout << "1";
+        break;
+    }
+    
     return 0;
 }
